c25096b87761c5bf6858af92526edf950335bbc7
Kaira Vain <kairavain@Kairas-MBP.lan>
2023-09-13 20:43:01 +0300

    add Week4Loops code

---
+++ WhileLoops/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActive = true;
+
+while (loopActive)
+{
+    cpuRandom = rnd.Next(1, 4);
+    //Console.Writeline($"cpu has generated {cpuRandom}.");
+    Console.WriteLine("Make aguess. Enter a number 1-3");
+
+2    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if (userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        //loopactive = false;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Oops. try again.");
+    }
+}
+
+Console.WriteLine("Have a nice day!");
